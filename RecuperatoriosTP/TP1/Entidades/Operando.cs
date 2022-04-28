15805ccf6a4482dc534f11eaf6b924e798a99132
Nicolas Penayo <penayonicolas@gmail.com>
2022-04-28 11:00:12 -0300

    Recuperatorio TP1

---
+++ RecuperatoriosTP/TP1/Entidades/Operando.cs
@@ -0,0 +1,169 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public Operando():this(0)
+        {
+        }
+        public Operando(double numero) : this(numero.ToString())
+        {
+   
+        }
+        public Operando(string numero)
+        {
+            Numero = numero;
+        }
+        private string Numero { set { this.numero = ValidarOperando(value); } }
+        /// <summary>
+        /// Corroborar que el operando sea válido
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns>Si es inválido devuelve 0</returns>
+        private double ValidarOperando(string strNumero)
+        {
+            if (double.TryParse(strNumero, out double input))
+            {
+                return input;
+            }
+            return 0;
+        }
+        /// <summary>
+        /// Corroborar que el numero este compuesto solo por "0" y "1"
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns></returns>
+        private bool EsBinario(string binario)
+        {
+            foreach (char digito in binario)
+            {
+                if (digito != '0' && digito != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Convertir un numero binario a decimal
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns>Si no es un numero binario devuelve "Valor inválido"</returns>
+        public string BinarioDecimal(string binario)
+        {
+            if (EsBinario(binario))
+            {
+                int resultado = 0;
+                int exponente = 0;
+                for (int i = binario.Length - 1; i >= 0; i--)
+                {
+                    int digito = binario[i] - 48;
+                    int potencia = (int)Math.Pow(2, exponente);
+                    resultado += digito * potencia;
+                    exponente++;
+                }
+                return resultado.ToString();
+            }
+            return "Valor inválido";
+        }
+        /// <summary>
+        /// Convertir un numero decimal a binario
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns>Si no es un numero binario devuelve "Valor inválido"</returns>
+        public string DecimalBinario(string numero)
+        {
+            if (int.TryParse(numero, out int numeroIngresado) && numeroIngresado > 0)
+            {
+                string resultado = "";
+                string digito;
+                string invertido = "";
+                int indice = 0;
+                int divisor = numeroIngresado;
+                while (divisor >= 2)
+                {
+                    digito = (divisor % 2).ToString();
+                    divisor = divisor / 2;
+                    resultado += digito;
+                    if (divisor < 2)
+                    {
+                        resultado += divisor;
+                    }
+                    indice++;
+                    //if(divisor == 2)
+                    //{
+                    //    resultado = resultado + digito;
+                    //    resultado = resultado + divisor/2;
+                    //}
+                }
+                for (int i = resultado.Length-1; i >= 0; i--)
+                {
+                    int j = 0;
+                    invertido = invertido + resultado[i].ToString();
+                    j++;
+                }
+
+                return invertido;
+
+            }
+            else
+            {
+                return "Valor inválido";
+            }
+        }
+        public string DecimalBinario(double numero)
+        {
+            return DecimalBinario(numero.ToString());
+        }
+        /// <summary>
+        /// Resta entre operandos
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+        /// <summary>
+        /// Producto de operandos
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+        /// <summary>
+        /// Division de operandos
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public static double operator /(Operando n1, Operando n2)
+        {
+            if (n2.numero == 0)
+            {
+                return double.MinValue;
+            }
+            else
+            {
+                return n1.numero / n2.numero;
+            }
+        }
+        /// <summary>
+        /// Suma de operandos
+        /// </summary>
+        /// <param name="n1"></param>
+        /// <param name="n2"></param>
+        /// <returns></returns>
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            Operando numero1 = new Operando(txtNumero1.Text);
+            Operando numero2 = new Operando(txtNumero2.Text);
+            string operador = cmbOperador.Text;
+            if(String.IsNullOrEmpty(operador))
+            {
+                operador = "+";
+            }
+            lblResultado.Text = Calculadora.Operar(numero1, numero2, operador[0]).ToString();
+            StringBuilder operaciones = new StringBuilder();
+            lstOperaciones.Items.Insert(0, operaciones.AppendLine($"{txtNumero1.Text} {operador[0]} {txtNumero2.Text} = {lblResultado.Text}"));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Operando resultado = new Operando();
+            lblResultado.Text = resultado.DecimalBinario(lblResultado.Text);
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Operando resultado = new Operando();
+            lblResultado.Text = resultado.BinarioDecimal(lblResultado.Text);
+        }
+        private void Limpiar()
+        {
+            this.lblResultado.Text = "0";
+            this.txtNumero1.Clear();
+            this.txtNumero2.Clear();
+            this.cmbOperador.SelectedIndex = 0;
+        }
+
+        private void FormCalculadora_Load(object sender, EventArgs e)
+        {
+            this.Limpiar();
+        }
+
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("¿Está seguro de que desea salir?", "Salir de la calculadora",MessageBoxButtons.YesNo,MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
